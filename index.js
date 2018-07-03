7015e9e0e3724ab27782d01b576f00a0a54ac418
Nicole Masarova <nicole.masarova@seasoned.co>
2018-07-03 11:01:46 -0700

    fetch api code js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-GET https://seasoned.atlassian.net/rest/agile/1.0/board/21/sprint/62
+	// Replace ./data.json with your JSON feed
+fetch('https://seasoned.atlassian.net/rest/agile/1.0/board/21/sprint/70/issue?fields=timetracking').then(response => {
+  return response.json();
+}).then(data => {
+  
+  console.log(data);
+}).catch(err => {
+  window.alert('Sorry. An error was thrown.')
+});
