cbb6cbd7202ca8b3dbc6d443792f14265055ee3a
Nicole Masarova <nicole.masarova@seasoned.co>
2018-07-05 16:17:21 -0700

    fetch json file

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+// 	// Replace ./data.json with your JSON feed
+// fetch('https://seasoned.atlassian.net/rest/agile/1.0/board/21/sprint/70/issue?fields=timetracking').then(response => {
+//   return response.json();
+// }).then(data => {
+  
+//   console.log(data);
+// }).catch(err => {
+//   window.alert('Sorry. An error was thrown.')
+// });
+
 	// Replace ./data.json with your JSON feed
-fetch('https://seasoned.atlassian.net/rest/agile/1.0/board/21/sprint/70/issue?fields=timetracking').then(response => {
+fetch('data.json').then(response => {
   return response.json();
 }).then(data => {
   
