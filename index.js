8a73a846169e74272bce1410769e2c06834814ff
Nicole Masarova <nicole.masarova@seasoned.co>
2018-07-18 09:08:19 -0700

    function - js

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
-	// Replace ./data.json with your JSON feed
-fetch('https://seasoned.atlassian.net/rest/agile/1.0/board/21/sprint/70/issue?fields=timetracking').then(response => {
-  return response.json();
-}).then(data => {
+// ATTEMPT 1
+// fetch('https://seasoned.atlassian.net/rest/agile/1.0/board/21/sprint/70/issue?fields=timetracking').then(response => {
+//   return response.json();
+// }).then(data => {
   
-  console.log(data);
-}).catch(err => {
-  window.alert('Sorry. An error was thrown.')
-});
+//   console.log(data);
+// }).catch(err => {
+//   window.alert('Sorry. An error was thrown.')
+// });
 
-	// Replace ./data.json with your JSON feed
-fetch('data.json').then(response => {
-  return response.json();
-}).then(data => {
+
+// ATTEMPT 2
+// fetch('data.json').then(response => {
+//   return response.json();
+// }).then(data => {
   
-  console.log(data);
-}).catch(err => {
-  window.alert('Sorry. An error was thrown.')
-});
+//   console.log(data);
+// }).catch(err => {
+//   window.alert('Sorry. An error was thrown.')
+// });
+
+get data.json .... //get the data file
+
+// FUNCTION
+
+let x = document.getElementById("ProjectNameInput");
+let y = document.getElementById("BoardNameInput");
+let timetracking = 'fields.timetracking';
+
+let onlyTimeTrackingArray = [];
+
+for(i=0, i<data.length, i++){
+	if x === 'issues.key' then(){
+			onlyTimeTrackingArray.push(data[i].fields.timetracking);
+	};
+};
+
+return onlyTimeTrackingArray;
