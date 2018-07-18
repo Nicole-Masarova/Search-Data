04a6e3af01915b93acd05318fda9a532e35edc16
Nicole Masarova <nicole.masarova@seasoned.co>
2018-07-18 08:48:42 -0700

    reorganizing the code (numbing out GET in JS and putting IDs in HTML)

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-// 	// Replace ./data.json with your JSON feed
-// fetch('https://seasoned.atlassian.net/rest/agile/1.0/board/21/sprint/70/issue?fields=timetracking').then(response => {
-//   return response.json();
-// }).then(data => {
+	// Replace ./data.json with your JSON feed
+fetch('https://seasoned.atlassian.net/rest/agile/1.0/board/21/sprint/70/issue?fields=timetracking').then(response => {
+  return response.json();
+}).then(data => {
   
-//   console.log(data);
-// }).catch(err => {
-//   window.alert('Sorry. An error was thrown.')
-// });
+  console.log(data);
+}).catch(err => {
+  window.alert('Sorry. An error was thrown.')
+});
 
 	// Replace ./data.json with your JSON feed
 fetch('data.json').then(response => {
